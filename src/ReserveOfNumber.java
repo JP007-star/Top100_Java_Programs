b421f283b971da3f9e643e1b912d5e8fb6dfbcc3
JP007-Star <justice_of_peace@yahoo.com>
2021-10-30 00:47:07 +0530

    Reverse of  a number

---
+++ src/ReserveOfNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReserveOfNumber {
+    public static void main(String[] args) {
+
+        //Scanner Obj Initialized
+        Scanner scanner=new Scanner(System.in);
+
+        //Prompt a number form user
+        System.out.println("Enter a number");
+        int num=scanner.nextInt();
+
+        int lastDigit=0;
+        String string="";
+
+        //Core Logic
+        while(num!=0){
+            lastDigit=num%10;
+            string=string+Integer.toString(lastDigit);
+            num=num/10;
+        }
+        System.out.println("The Reverse of a Number is:"+string);
+    }
+}
