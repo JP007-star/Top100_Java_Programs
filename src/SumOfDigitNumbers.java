04dace99a8661ce5aa8d1297a449a6bef90b9412
JP007-Star <justice_of_peace@yahoo.com>
2021-10-30 00:26:38 +0530

    Sum of Digits of number

---
+++ src/SumOfDigitNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfDigitNumbers {
+    public static void main(String[] args) {
+
+        //Scanner Obj Initialized
+        Scanner scanner=new Scanner(System.in);
+
+        //Prompt a number form user
+        System.out.println("Enter a number");
+        int num=scanner.nextInt();
+
+        int lastDigit=0;
+        int sum=0;
+
+        //Core Logic
+        while(num!=0){
+            lastDigit=num%10;
+            sum=sum+lastDigit;
+            num=num/10;
+        }
+       System.out.println("The Sum of Given Digit of Number is:"+sum);
+    }
+}
