ba7a2b3facf35cc83d9b04150957528882042d3d
JP007-Star <justice_of_peace@yahoo.com>
2021-11-12 22:39:43 +0530

    Vowel Count

---
+++ src/CountVowelArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountVowelArray {
+    public static void main(String[] args) {
+        //Scanner Obj Initialized
+        Scanner scanner=new Scanner(System.in);
+
+        //prompt  2 numbers from user
+        System.out.println("Enter a String:");
+        String string=scanner.next();
+        char charArray[]=string.toCharArray();
+        int vowel=0;
+        for(int i=0;i<charArray.length;i++){
+            if(string.charAt(i)=='a'||string.charAt(i)=='e'|| string.charAt(i)=='i'||string.charAt(i)=='o'||string.charAt(i)=='u'||
+                    string.charAt(i)=='A'||string.charAt(i)=='E'|| string.charAt(i)=='I'||string.charAt(i)=='O'||string.charAt(i)=='U'){
+                vowel++;
+            }
+        }
+        System.out.println("No of Vowels Present in this String :"+vowel);
+    }
+}
