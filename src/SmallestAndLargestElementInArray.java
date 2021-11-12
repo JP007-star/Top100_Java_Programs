b25085ad2c9eb9c32c7aacac90fd195a53aa38b7
JP007-Star <justice_of_peace@yahoo.com>
2021-11-12 18:03:45 +0530

    SmallestAndLargestElementInArray

---
+++ src/SmallestAndLargestElementInArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SmallestAndLargestElementInArray {
+    public static void main(String[] args) {
+        //Scanner Obj Initialized
+        Scanner scanner=new Scanner(System.in);
+
+        //Prompt a length form user
+        System.out.println("Enter the length of Array:");
+        int arrayLength=scanner.nextInt();
+
+        //
+        int array[]=new int[200];
+        System.out.println("Enter the elements in Array");
+        for(int i=0;i<arrayLength;i++){
+            array[i]=scanner.nextInt();
+        }
+
+        int temp=0;
+        for (int i=0;i<arrayLength;i++){
+            for(int j=i+1;j<arrayLength;j++){
+                if(array[i]>array[j]){
+                    temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+
+        System.out.println("Smallest Element in Array:"+array[0]);
+        System.out.println("Largest Element in Array:"+array[arrayLength-1]);
+    }
+}
