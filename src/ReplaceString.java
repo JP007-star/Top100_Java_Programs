591770f3d02bec5c168445e000cf928a91c072c2
JP007-Star <justice_of_peace@yahoo.com>
2021-11-13 18:14:25 +0530

    Replacing String

---
+++ src/ReplaceString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReplaceString {
+    public static void main(String[] args) {
+        //Scanner Obj Initialized
+        Scanner scanner=new Scanner(System.in);
+
+        //Prompt a number form user
+        System.out.println("Enter a string:");
+        String string=scanner.next();
+
+        System.out.println("Enter a string to be replaced:");
+        String oldString=scanner.next();
+
+        System.out.println("Enter a string for replacing:");
+        String newString=scanner.next();
+
+
+
+        String resultantString=string.replace(oldString,newString);
+        System.out.println("New String is:"+resultantString);
+    }
+}
